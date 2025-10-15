ff9f6a9d7289036498c22d3332d5e7546999e3c0
iamonlyacapitan-cpu <iamonlyacapitan@gmail.com>
2025-10-15 15:52:31 +0330

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ if not BOT_TOKEN:
     raise Exception("BOT_TOKEN محیطی تنظیم نشده!")
 
 ADMIN_ID = int(os.environ.get("ADMIN_ID") or 1606291654)
+
 DATABASE_URL = os.environ.get("DATABASE_URL")
 if not DATABASE_URL:
     raise Exception("DATABASE_URL محیطی تنظیم نشده!")
